9c805fdbf907783d0e34c25445576d9d51d7a596
Sarvesh Singh <sarvesh2296@gmail.com>
2017-07-08 11:00:51 +0530

    Update Wave_Array.cpp

--- Wave_Array.cpp
+++ Wave_Array.cpp
@@ -10,11 +10,7 @@ One possible answer : [2, 1, 4, 3]
 Another possible answer : [4, 1, 3, 2]
 */
 vector<int> Solution::wave(vector<int> &A) {
-    // Do not write main() function.
-    // Do not read input, instead use the arguments to the function.
-    // Do not print the output, instead return values as specified
-    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-    int n;
+    
     int largest = 0;
     int temp;
     n = A.size();
